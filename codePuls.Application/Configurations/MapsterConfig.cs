aa5605633a649b3074dda27376f6a9d054d171cd
Nimeth Nimdinu <INT.Nimeth@CreativeSoftware.com>
2025-07-15 15:20:19 +0530

    solution and build

--- codePuls.Application/Configurations/MapsterConfig.cs
+++ codePuls.Application/Configurations/MapsterConfig.cs
@@ -20,51 +20,51 @@ namespace codePuls.Application.Configurations
                                 src.DisplayName,
                                 src.ProjectCreatedAt,
                                 src.ProjectUpdatedAt)); 
-            TypeAdapterConfig<User, UserResponseDto>.NewConfig();
-            TypeAdapterConfig<UserRequestDto, User>.NewConfig()
-                            .ConstructUsing(src => new User(
-                                src.UserName, 
-                                src.NodeId,
-                                src.AvatarUrl,
-                                src.DisplayName,
-                                src.UserCreatedAt,
-                                src.UserUpdatedAt));
-            TypeAdapterConfig<Team, TeamResponseDto>.NewConfig();
-            TypeAdapterConfig<TeamRequestDto, Team>.NewConfig()
-                .ConstructUsing(src => new Team(
-                    src.NodeId,      
-                    src.TeamName,
-                    src.Description
-                    //src.ProjectId
-                ));
-            TypeAdapterConfig<TeamMemberRequestDto, TeamMember>.NewConfig();
+            //TypeAdapterConfig<User, UserResponseDto>.NewConfig();
+            //TypeAdapterConfig<UserRequestDto, User>.NewConfig()
+            //                .ConstructUsing(src => new User(
+            //                    src.UserName, 
+            //                    src.NodeId,
+            //                    src.AvatarUrl,
+            //                    src.DisplayName,
+            //                    src.UserCreatedAt,
+            //                    src.UserUpdatedAt));
+            //TypeAdapterConfig<Team, TeamResponseDto>.NewConfig();
+            //TypeAdapterConfig<TeamRequestDto, Team>.NewConfig()
+            //    .ConstructUsing(src => new Team(
+            //        src.NodeId,      
+            //        src.TeamName,
+            //        src.Description
+            //        //src.ProjectId
+            //    ));
+            //TypeAdapterConfig<TeamMemberRequestDto, TeamMember>.NewConfig();
 
 
-            TypeAdapterConfig<CodeRepository, CodeRepositoryRequestDto>.NewConfig();
-            TypeAdapterConfig<CodeRepositoryRequestDto, CodeRepository>.NewConfig().ConstructUsing(src => new CodeRepository(
-                                src.NodeId,
-                                src.CodeRepositoryName,
-                                src.FullName,
-                                src.ProjectId
-                                ));
-            TypeAdapterConfig<PullRequest, PullRequestRequestDto>.NewConfig();
-            TypeAdapterConfig<PullRequestRequestDto, PullRequest>.NewConfig()
-                            .ConstructUsing(src => new PullRequest(
-                                src.NodeId,
-                                src.Number,
-                                src.State,
-                                src.Commits,
-                                src.Additions,
-                                src.Deletions,
-                                src.ChangedFiles,
-                                src.PRCreatedAt,
-                                src.PRUpdatedAt,
-                                src.PRMergedAt,
-                                src.PRClosedAt,
-                                src.CodeRepositoryId,
-                                src.UserId,
-                                src.ProjectId
-                            ));
+            //TypeAdapterConfig<CodeRepository, CodeRepositoryRequestDto>.NewConfig();
+            //TypeAdapterConfig<CodeRepositoryRequestDto, CodeRepository>.NewConfig().ConstructUsing(src => new CodeRepository(
+            //                    src.NodeId,
+            //                    src.CodeRepositoryName,
+            //                    src.FullName,
+            //                    src.ProjectId
+            //                    ));
+            //TypeAdapterConfig<PullRequest, PullRequestRequestDto>.NewConfig();
+            //TypeAdapterConfig<PullRequestRequestDto, PullRequest>.NewConfig()
+            //                .ConstructUsing(src => new PullRequest(
+            //                    src.NodeId,
+            //                    src.Number,
+            //                    src.State,
+            //                    src.Commits,
+            //                    src.Additions,
+            //                    src.Deletions,
+            //                    src.ChangedFiles,
+            //                    src.PRCreatedAt,
+            //                    src.PRUpdatedAt,
+            //                    src.PRMergedAt,
+            //                    src.PRClosedAt,
+            //                    src.CodeRepositoryId,
+            //                    src.UserId,
+            //                    src.ProjectId
+            //                ));
         }
     }
 }
