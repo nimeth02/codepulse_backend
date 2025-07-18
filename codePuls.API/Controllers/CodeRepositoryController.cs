350e19ee3510913cc39718dd1ef98bfa4aeb95ae
Nimeth Nimdinu <INT.Nimeth@CreativeSoftware.com>
2025-07-18 15:50:02 +0530

    codeRepository service

---
+++ codePuls.API/Controllers/CodeRepositoryController.cs
@@ -0,0 +1,44 @@
+﻿using codePuls.Application.Common.ResponseModels;
+using codePuls.Application.DTOs.RequestDTOs;
+using codePuls.Application.DTOs.ResponseDTOs;
+using codePuls.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace codePuls.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CodeRepositoryController(ILogger<CodeRepositoryController> logger, ICodeRepositoryService codeRepositoryService) : ControllerBase
+    {
+        private readonly ILogger<CodeRepositoryController> _logger = logger;
+        private readonly ICodeRepositoryService _codeRepositoryService = codeRepositoryService;
+
+        [HttpGet("{projectId}")]
+        public async Task<ActionResult> GetAllByProjectIdAsync(Guid ProjectId)
+        {
+            var codeRepositories = await _codeRepositoryService.GetAllByProjectIdAsync(ProjectId);
+            var response = new SuccessResponse<IEnumerable<CodeRepositoryResponseDto>>
+            {
+                Data = codeRepositories,
+                Message = "Repositories retrieved  successfully.",
+                StatusCode = StatusCodes.Status200OK,
+                Success = true
+            };
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateRepositoriesAsync(CreateCodeRepositoriesDto request)
+        {
+            var codeRepositories = await _codeRepositoryService.CreateRepositoriesAsync(request.ProjectId, request.CodeRepositories);
+            var response = new SuccessResponse<IEnumerable<CodeRepositoryResponseDto>>
+            {
+                Data = codeRepositories,
+                Message = "Repositories created  successfully.",
+                StatusCode = StatusCodes.Status201Created,
+                Success = true
+            };
+            return Created("api/codeRepository", response);
+        }
+    }
+}
---
+++ codePuls.Application/DTOs/RequestDTOs/CreateCodeRepositoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codePuls.Application.DTOs.RequestDTOs
+{
+    public record class CreateCodeRepositoriesDto
+    {
+        public Guid ProjectId { get; init; }
+        public CodeRepositoryRequestDto[] CodeRepositories { get; init; }
+    }
+}
